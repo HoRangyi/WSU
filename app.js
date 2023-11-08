9f6387d990a033307b4f3a0c554690ddef9476cf
HoRangyi <92179030+HoRangyi@users.noreply.github.com>
2023-11-08 19:44:49 +0900

    hihi

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //#region 사용중인 모듈
-const express = require("express") // 웹홈페이지 모듈
-const app = express() // 모듈 별칭화
-const sprintf = require("sprintf-js").sprintf // sprintf 사용모듈
-const bodyParser = require("body-parser") // 미들웨어 모듈
-const maria = require("./DB/maria") //DB연결모듈
+const express = require("express"); // 웹홈페이지 모듈
+const app = express(); // 모듈 별칭화
+const sprintf = require("sprintf-js").sprintf; // sprintf 사용모듈
+const bodyParser = require("body-parser"); // 미들웨어 모듈
+const maria = require("./DB/maria"); //DB연결모듈
 const port = 3123; // 포트번호
 //#endregion
 
@@ -22,17 +22,23 @@ app.post("/message", function (req, res) {
 //#endregion
 
 //#region 미들웨어 적용(static)
-app.set("view engine", "ejs")
-app.use(express.static("./view" + "/"))
+app.set("view engine", "ejs");
+app.use(express.static("./view" + "/"));
 app.use(express.json());
 
 app.use(bodyParser.text());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/situation_board", (req, res) => {
+  // 절대 경로를 사용하여 파일을 직접 제공합니다.
+  res.sendFile("C:/Users/user/Downloads/sv3/WSU/view/situation_board.html");
+});
+
 //#endregion
 
 //기본 홈페이지 랜더링
 app.get("/", (req, res) => {
-  res.render("index", {title:'express'});
+  res.render("index", { title: "express" });
 });
 
 //#region 상황판
@@ -52,20 +58,18 @@ app.get("/", (req, res) => {
 
 //#endregion
 
-
-
 // DB 라인
 //#region 사용자 등록
 app.post("/sign_up", function (req, res) {
-  const u_id = req.body.user_id       // 유저 ID
-  const u_pw = req.body.user_pw       // 유저 PW
-  const u_name = req.body.user_name   // 유저 이름
-  const u_phone = req.body.user_phone //유저 전화번호
-  const u_addr = req.body.user_adr    // 유저 주소
-  const u_gardian_name = ""           // 보호자 이름
-  const u_gardian_phone = ""          // 보호자 번호
-  const u_offices = ""                // 관공서
-  const u_chief = ""                  // 이장님
+  const u_id = req.body.user_id; // 유저 ID
+  const u_pw = req.body.user_pw; // 유저 PW
+  const u_name = req.body.user_name; // 유저 이름
+  const u_phone = req.body.user_phone; //유저 전화번호
+  const u_addr = req.body.user_adr; // 유저 주소
+  const u_gardian_name = ""; // 보호자 이름
+  const u_gardian_phone = ""; // 보호자 번호
+  const u_offices = ""; // 관공서
+  const u_chief = ""; // 이장님
 
   var SQL = sprintf(
     'INSERT INTO basic VALUES("%s", "%s", "%s","%s","%s","%s","%s","%s","%s");',
@@ -91,18 +95,25 @@ app.post("/sign_up", function (req, res) {
 //#endregion
 app.post("/sign_in", function (req, res) {
   const u_id = req.body.user_id;
-  //const u_pw = req.body.user_pw;
+  const u_pw = req.body.user_pw;
 
   var SQL = sprintf("Select id from member where id = '%s'", u_id);
 
+  if (u_id == "admin" && u_pw == "admin") {
+    // 관리자 로그인이면 관리자 페이지로 리다이렉션
+    res.redirect("/admin_page_path");
+  } else {
     maria.query(SQL, function (err, rows, fields) {
-      if (!err) {
-          res.render("/view/situation_board.html")
+      if (!err && rows.length > 0) {
+        // 로그인이 성공하면 situation_board.html 페이지로 리다이렉션
+        res.redirect("/situation_board");
       } else {
+        // 로그인 실패시 실패 메시지 전송
         res.send("로그인 실패..");
         console.log("[DB] INSERT ERROR!");
       }
     });
+  }
 });
 
 app.listen(port, () => {
