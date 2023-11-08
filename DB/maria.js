9985c55989e1386ee7f6403787f56e7079d7606b
HoRangyi <92179030+HoRangyi@users.noreply.github.com>
2023-11-08 18:47:37 +0900

    159

--- DB/maria.js
+++ DB/maria.js
@@ -1,13 +1,13 @@
-const maria = require('mysql')
+const maria = require("mysql");
 
-var host_number = '172.16.3.248'
+var host_number = "172.16.0.135";
 
 const conn = maria.createConnection({
-    host: host_number,
-    port:3306,
-    user:'tester',
-    password:'5340',
-    database:'myproject'
-})
+  host: host_number,
+  port: 3306,
+  user: "tester",
+  password: "5340",
+  database: "myproject",
+});
 
-module.exports = conn
+module.exports = conn;
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //#region 사용중인 모듈
-const express = require("express") // 웹홈페이지 모듈
-const app = express() // 모듈 별칭화
-const sprintf = require("sprintf-js").sprintf // sprintf 사용모듈
-const bodyParser = require("body-parser") // 미들웨어 모듈
-const maria = require("./DB/maria") //DB연결모듈
+const express = require("express"); // 웹홈페이지 모듈
+const app = express(); // 모듈 별칭화
+const sprintf = require("sprintf-js").sprintf; // sprintf 사용모듈
+const bodyParser = require("body-parser"); // 미들웨어 모듈
+const maria = require("./DB/maria"); //DB연결모듈
 const port = 3123; // 포트번호
 //#endregion
 
@@ -22,8 +22,8 @@ app.post("/message", function (req, res) {
 //#endregion
 
 //#region 미들웨어 적용(static)
-app.set("view engine", "ejs")
-app.use(express.static("./view" + "/"))
+app.set("view engine", "ejs");
+app.use(express.static("./view" + "/"));
 app.use(express.json());
 
 app.use(bodyParser.text());
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //기본 홈페이지 랜더링
 app.get("/", (req, res) => {
-  res.render("index", {title:'express'});
+  res.render("index", { title: "express" });
 });
 
 //#region 상황판
@@ -52,20 +52,18 @@ app.get("/", (req, res) => {
 
 //#endregion
 
-
-
 // DB 라인
 //#region 사용자 등록
 app.post("/sign_up", function (req, res) {
-  const u_id = req.body.user_id       // 유저 ID
-  const u_pw = req.body.user_pw       // 유저 PW
-  const u_name = req.body.user_name   // 유저 이름
-  const u_phone = req.body.user_phone //유저 전화번호
-  const u_addr = req.body.user_adr    // 유저 주소
-  const u_gardian_name = ""           // 보호자 이름
-  const u_gardian_phone = ""          // 보호자 번호
-  const u_offices = ""                // 관공서
-  const u_chief = ""                  // 이장님
+  const u_id = req.body.user_id; // 유저 ID
+  const u_pw = req.body.user_pw; // 유저 PW
+  const u_name = req.body.user_name; // 유저 이름
+  const u_phone = req.body.user_phone; //유저 전화번호
+  const u_addr = req.body.user_adr; // 유저 주소
+  const u_gardian_name = ""; // 보호자 이름
+  const u_gardian_phone = ""; // 보호자 번호
+  const u_offices = ""; // 관공서
+  const u_chief = ""; // 이장님
 
   var SQL = sprintf(
     'INSERT INTO basic VALUES("%s", "%s", "%s","%s","%s","%s","%s","%s","%s");',
@@ -95,18 +93,26 @@ app.post("/sign_in", function (req, res) {
 
   var SQL = sprintf("Select id from emtest where id = %s", u_id);
 
-  if (u_id == "admin" && u_pw == "admin") {
-    res.send("어드민계정");
-  } else {
-    maria.query(SQL, function (err, rows, fields) {
-      if (!err) {
-        res.send("로그인 성공!");
-      } else {
-        res.send("로그인 실패..");
-        console.log("[DB] INSERT ERROR!");
-      }
-    });
-  }
+  app.post("/login", function (req, res) {
+    var u_id = req.body.id;
+    var u_pw = req.body.password;
+
+    if (u_id == "admin" && u_pw == "admin") {
+      res.send("어드민계정");
+    } else {
+      maria.query(SQL, function (err, rows, fields) {
+        if (!err) {
+          // 로그인 성공 시, HTML 파일을 클라이언트에게 보냅니다.
+          res.sendFile(
+            "C:/Users/user/Downloads/sv3/WSU/view/situation board.html"
+          );
+        } else {
+          res.send("로그인 실패..");
+          console.log("[DB] INSERT ERROR!");
+        }
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
