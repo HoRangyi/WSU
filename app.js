55800d1ccd3e476d892c04811028623d3f1b1b30
I-want-going-home <alsrb2775@naver.com>
2023-12-06 17:42:05 +0900

    backendupdate

--- app.js
+++ app.js
@@ -5,49 +5,36 @@ const sprintf = require("sprintf-js").sprintf; // sprintf 사용모듈
 const bodyParser = require("body-parser"); // 미들웨어 모듈
 const maria = require("./DB/maria"); //DB연결모듈
 const port = 3123; // 포트번호
-const fs = require("fs");
+const fs = require("fs")
 
 const server = app.listen(port, () => {
   console.log("3123 포트에 로컬로 서버가열렸어요!");
 });
 
-const io = require("socket.io")(server);
+const io = require("socket.io")(server)
 
 //#endregion
-// 데이터베이스 대신 메모리에 임시로 저장할 객체
-let profile = {
-  username: "초기 사용자 이름",
-  address: "초기 주소",
-};
 
 // bodyParser 미들웨어를 사용하여 JSON 요청 본문을 파싱합니다.
 app.use(bodyParser.json());
-
-// 프로필 데이터를 가져오는 라우트
-app.get("/profile", (req, res) => {
-  res.json(profile);
-});
-
-// 프로필 데이터를 업데이트하는 라우트
-app.post("/update-profile", (req, res) => {
-  const { username, address } = req.body;
-  profile.username = username;
-  profile.address = address;
-  res.send({ message: "프로필이 성공적으로 업데이트되었습니다." });
-});
 
 //#region 파이썬 함수 호출
 const { spawn } = require("node:child_process");
 const { title } = require("node:process");
 
-//메세지전송
-app.post("/message", function (req, res) {
-  const ls = spawn("python", ["test2.py"]);
+var lock = true;
+function Disaster_s(data){
+  if(lock == true){
+  if(data == "1"){
+    const ls = spawn("python", ["test2.py"]);
+      ls.stdout.on("data", (data) => {
+        console.log("stdout: ${data}");
+    });
+  }
+  lock = false;
+ }
+}
 
-  ls.stdout.on("data", (data) => {
-    console.log("stdout: ${data}");
-  });
-});
 //#endregion
 
 //#region 미들웨어 적용(static)
@@ -64,39 +51,64 @@ app.get("/situation_board", (req, res) => {
 
 //#endregion
 
-//#region socket
-io.on("connection", (socket) => {
-  setInterval(() => {
-    image_path((err, data) => {
-      if (err) {
-        console.error(err);
-      } else {}
-    });
+
+io.on('connection', (socket) => {
+  
+  updateValues();
+  
+  const intervalId = setInterval(() => {
+    updateValues();
   }, 1000);
 
-  socket.on("disconnect", () => {
-    console.log("연결종료");
+  socket.on('disconnect', () => {
+    clearInterval(intervalId);
+    console.log('연결종료');
   });
 });
+
+var imagePaths = [];
+var serials = [];
+
+function updateValues() {
+  image_path((err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+        imagePaths.push(data);
+        io.emit('image_path_1', imagePaths[0]);
+        io.emit('image_path_2', imagePaths[1]);
+        imagePaths = [data]; // 배열안에 data.length 이상 속성이있으면 초기화
+    }
+  });
+  serial_path((err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      serials = data;
+      io.emit('serial_path_1', serials[0]);
+      io.emit('serial_path_2', serials[1]);
+      serials = [data]    // 배열안에 data.length 이상 속성이있으면 초기화
+    }
+  });
+}
 
 function image_path(callback) {
   var SQL = sprintf("Select * from ai");
 
   maria.query(SQL, function (err, rows, fields) {
     if (!err && rows.length > 0) {
-      var serials = [];
       var disasters = [];
       var num = 0;
       for (const row of rows) {
-        serials[num] = row.Sirial;
         disasters[num] = row.Disaster;
        
-        var newImageSrc = "";  
-      
+        var newImageSrc = "";
+
         if (disasters[num] == 0) {
           newImageSrc = "0.png";
         } else if (disasters[num] == 1) {
           newImageSrc = "1.png";
+          Disaster_s(disasters[num])
         } else if (disasters[num] == 2) {
           newImageSrc = "2.png";
         } else if (disasters[num] == 3) {
@@ -110,11 +122,29 @@ function image_path(callback) {
         } else if (disasters[num] == 7) {
           newImageSrc = "7.png";
         }
-  
         // 이미지 파일 경로를 콜백으로 전달
         callback(null, newImageSrc); 
       }
+      num++;
+    }
+  });
+}
 
+
+function serial_path(callback) {
+  var SQL = sprintf("Select * from ai");
+
+  maria.query(SQL, function (err, rows, fields) {
+    if (!err && rows.length > 0) {
+      var serials = [];
+      for (const row of rows) {
+        serials.push(row.Sirial);
+      }
+      // 시리얼 값을 배열 전체로 콜백으로 전달
+      callback(null, serials);
+    }
+  });
+}
 //#endregion
 
 //기본 홈페이지 랜더링
@@ -128,24 +158,16 @@ app.post("/sign_up", function (req, res) {
   const u_id = req.body.user_id; // 유저 ID
   const u_pw = req.body.user_pw; // 유저 PW
   const u_name = req.body.user_name; // 유저 이름
-  const u_phone = req.body.user_phone; //유저 전화번호
   const u_addr = req.body.user_adr; // 유저 주소
-  const u_gardian_name = ""; // 보호자 이름
-  const u_gardian_phone = ""; // 보호자 번호
-  const u_offices = ""; // 관공서
-  const u_chief = ""; // 이장님
+  const u_phone = req.body.user_phone; //유저 전화번호
 
   var SQL = sprintf(
-    'INSERT INTO basic VALUES("%s", "%s", "%s","%s","%s","%s","%s","%s","%s");',
+    'INSERT INTO member VALUES("","%s", "%s", "%s","%s","%s");',
     u_id,
     u_pw,
     u_name,
-    u_phone,
     u_addr,
-    u_gardian_name,
-    u_gardian_phone,
-    u_offices,
-    u_chief
+    u_phone
   );
   maria.query(SQL, function (err, rows, fields) {
     if (!err) {
@@ -161,7 +183,7 @@ app.post("/sign_in", function (req, res) {
   const u_id = req.body.user_id;
   const u_pw = req.body.user_pw;
 
-  var SQL = sprintf("Select id from member where id = '%s'", u_id);
+  var SQL = sprintf("Select id, pw from member where id = '%s' and pw = '%s'", u_id, u_pw);
 
   maria.query(SQL, function (err, rows, fields) {
     if (!err && rows.length > 0) {
@@ -174,4 +196,6 @@ app.post("/sign_in", function (req, res) {
     }
   });
 });
+
+
 
