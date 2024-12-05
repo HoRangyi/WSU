4e5cde4184a1764ca321247a47760d59a0cbe037
HoRangyi <92179030+HoRangyi@users.noreply.github.com>
2024-12-05 13:58:45 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 def send_sms(to, body):
     # Twilio 계정 정보
-    account_sid = 'AC927e0d07a453d64e08b7fe703452fcfa'
-    auth_token = '156ab2aed5032062b6ebdebbf6c2656e'
+    account_sid = ''
+    auth_token = ''
 
     # Twilio 클라이언트 초기화
     client = Client(account_sid, auth_token)
@@ -11,7 +11,7 @@ def send_sms(to, body):
     try:
         # SMS 메시지 보내기
         message = client.messages.create(
-            from_='+18589433800',  # Twilio에서 제공하는 전화번호
+            from_='+',  # Twilio에서 제공하는 전화번호
             body=body,
             to=to  # 수신자 전화번호
         )
@@ -21,7 +21,7 @@ def send_sms(to, body):
         # 오류가 발생하면 오류 메시지를 반환합니다.
         return str(e)
     
-to_phone_number = '+821062346995'
+to_phone_number = ''
 message_body = '오류가 너무싫다'
 result = send_sms(to_phone_number, message_body)
 
