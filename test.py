3c85204bc06d1e11efa7f3d93772cf41b87d1773
HoRangyi <92179030+HoRangyi@users.noreply.github.com>
2024-12-05 13:58:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 from twilio.rest import Client
 
-account_sid = 'AC927e0d07a453d64e08b7fe703452fcfa'
-auth_token = 'bba8614c57e3b88a8f6931cbd4c0784a'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 def Message():
     message = client.messages.create(
-    from_='+18124962951',
+    from_='+',
     body='1',
-    to='+821062346995'
+    to='+'
     )
 
     print(message.sid)
